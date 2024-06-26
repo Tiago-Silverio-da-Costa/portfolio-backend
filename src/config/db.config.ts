d23f62859f90a617205b6a476ecbf3f29a6a666e
Tiago-Silverio-da-Costa <tiagosilveriodacosta@gmail.com>
2024-06-26 16:07:56 -0300

    back to db

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -2,17 +2,16 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export default {
-    DB: process.env.POSTGRES_DATABASE as string,
-    USER: process.env.POSTGRES_USER as string,
-    PASSWORD: process.env.POSTGRES_PASSWORD as string,
-    HOST: process.env.POSTGRES_HOST as string,
-    dialect: 'postgres', 
-    URL: process.env.POSTGRES_URL as string,
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
-    }
+  DB: process.env.DATABASE,
+  USER: process.env.SUPABASE_SERVICE_ROLE,
+  PASSWORD: process.env.SUPABASE_SERVICE_PASSWORD,
+  HOST: process.env.SUPABASE_SERVICE_HOST,
+  dialect: 'postgres',
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
 
 }
