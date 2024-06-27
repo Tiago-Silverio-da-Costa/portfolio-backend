6b1b87e37aff932555539f3e0723afdfb64a23ca
Tiago-Silverio-da-Costa <tiagosilveriodacosta@gmail.com>
2024-06-27 01:58:53 -0300

    ,..

--- public/api/index.js
+++ public/api/index.js
@@ -5,9 +5,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
-const models_1 = __importDefault(require("./models"));
-const auth_routes_1 = __importDefault(require("./routes/auth.routes"));
-const user_routes_1 = __importDefault(require("./routes/user.routes"));
+const models_1 = __importDefault(require("../utils/models"));
+const auth_routes_1 = __importDefault(require("../utils/routes/auth.routes"));
+const user_routes_1 = __importDefault(require("../utils/routes/user.routes"));
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)({
     origin: "https://portfolio-tiagosc.vercel.app",
